4449c42a32cec11a90aaccc360da2e79313a4528
PedroLCamara <plcrlschool@gmail.com>
2021-09-16 17:38:08 -0300

    InterfaceTipoUsuario

---
+++ API/senai.HROADS.webAPI/senai.HROADS.webAPI/Interfaces/ITipoUsuarioRepository.cs
@@ -0,0 +1,21 @@
+﻿using senai.HROADS.webAPI.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.HROADS.webAPI.Interfaces
+{
+    interface ITipoUsuarioRepository
+    {
+        //C
+        void Cadastrar(TipoUsuario NovoTipo);
+        //R
+        TipoUsuario BuscarPorId(int IdTipoUsuario);
+        List<TipoUsuario> ListarTodos();
+        //U
+        void Atualizar(TipoUsuario TipoAtualizado, int IdTipoAtualizado);
+        //D
+        void Deletar(int IdTipoDeletado);
+    }
+}
