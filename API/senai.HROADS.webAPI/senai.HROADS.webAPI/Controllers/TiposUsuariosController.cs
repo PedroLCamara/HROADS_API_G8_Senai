8cf0f3d561abf343c2056ce129e2b3d971164388
NatanPereiraFurtado <felipexgamer6@gmail.com>
2021-09-17 16:48:14 -0300

    Hugo - Mandando o progresso que você fez antes de ir fazer a prova kk

--- API/senai.HROADS.webAPI/senai.HROADS.webAPI/Controllers/TiposUsuariosController.cs
+++ API/senai.HROADS.webAPI/senai.HROADS.webAPI/Controllers/TiposUsuariosController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using senai.HROADS.webAPI.Domains;
+using senai.HROADS.webAPI.Interfaces;
+using senai.HROADS.webAPI.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +10,63 @@ using System.Threading.Tasks;
 
 namespace senai.HROADS.webAPI.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class TiposUsuariosController : ControllerBase
     {
+        ITipoUsuarioRepository TpURepositorio;
+
+        public TiposUsuariosController()
+        {
+            TpURepositorio = new TipoUsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult ListarTodos()
+        {
+            try
+            {
+                return Ok(TpURepositorio.ListarTodos());
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+                throw;
+            }
+        }
+        [HttpGet("IdTpUsuarioBuscado")]
+        public IActionResult BuscarPorId(int IdTpUsuarioBuscado)
+        {
+            try
+            {
+                if (TpURepositorio.BuscarPorId(IdTpUsuarioBuscado) != null)
+                {
+                    return Ok(TpURepositorio.BuscarPorId(IdTpUsuarioBuscado));
+                }
+                else return NotFound("Id não encontrado!!!");
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+                throw;
+            }
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(TipoUsuario NovoTpUsuario)
+        {
+            try
+            {
+                TpURepositorio.Cadastrar(NovoTpUsuario);
+                return StatusCode(201);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+                throw;
+            }
+        }
+        [HttpPut]
+        public IActionResult 
     }
 }
